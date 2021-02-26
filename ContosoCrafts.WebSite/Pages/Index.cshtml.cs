497fd07b78e1ac9867e0f3ac55b126230c027faf
AndyJPG <jiangpeigeng@gmail.com>
2021-02-26 20:46:50 +1100

    Retrieve data from root data folder json file. Deserialize the data into product object. Add json product service in Index cshtml cs file and get display to page.

--- ContosoCrafts.WebSite/Pages/Index.cshtml.cs
+++ ContosoCrafts.WebSite/Pages/Index.cshtml.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using System;
@@ -11,15 +13,19 @@ namespace ContosoCrafts.WebSite.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        public JsonFileProductService ProductService;
+        public IEnumerable<Product> Products { get; private set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger,
+            JsonFileProductService productService)
         {
             _logger = logger;
+            ProductService = productService;
         }
 
         public void OnGet()
         {
-
+            Products = ProductService.GetProducts();
         }
     }
 }
--- ContosoCrafts.WebSite/Services/JsonFileProductService.cs
+++ ContosoCrafts.WebSite/Services/JsonFileProductService.cs
@@ -20,7 +20,7 @@ namespace ContosoCrafts.WebSite.Services
 
         private string JsonFileName
         {
-            get { return Path.Combine(WebHostEnvironment.WebRootPath, "date", "products.json"); }
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "products.json"); }
         }
 
         public IEnumerable<Product> GetProducts()
