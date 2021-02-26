8e6cab3b3225e7209d9fddd033a1701cc37e40ef
AndyJPG <jiangpeigeng@gmail.com>
2021-02-26 23:52:31 +1100

    Add method to update rating from query string using get request.

--- ContosoCrafts.WebSite/Controllers/ProductsController.cs
+++ ContosoCrafts.WebSite/Controllers/ProductsController.cs
@@ -25,5 +25,16 @@ namespace ContosoCrafts.WebSite.Controllers
         {
             return ProductService.GetProducts();
         }
+
+        //[HttpPatch] "[FromBody]"
+        [Route("Rate")]
+        [HttpGet]
+        public ActionResult Get(
+            [FromQuery] string ProductId, 
+            [FromQuery] int Rating)
+        {
+            ProductService.AddRating(ProductId, Rating);
+            return Ok();
+        }
     }
 }
--- ContosoCrafts.WebSite/Services/JsonFileProductService.cs
+++ ContosoCrafts.WebSite/Services/JsonFileProductService.cs
@@ -34,5 +34,36 @@ namespace ContosoCrafts.WebSite.Services
                     });
             }
         }
+
+        public void AddRating(string productId, int rating)
+        {
+            IEnumerable<Product> products = GetProducts();
+
+            //LINQ
+            Product query = products.First(x => x.Id == productId);
+
+            if (query.Ratings == null)
+            {
+                query.Ratings = new int[] { rating };
+            }
+            else
+            {
+                List<int> ratings = query.Ratings.ToList();
+                ratings.Add(rating);
+                query.Ratings = ratings.ToArray();
+            }
+
+            using (var outputStream = File.OpenWrite(JsonFileName))
+            {
+                JsonSerializer.Serialize<IEnumerable<Product>>(
+                    new Utf8JsonWriter(outputStream, new JsonWriterOptions
+                    {
+                        SkipValidation = true,
+                        Indented = true
+                    }),
+                    products
+                );
+            }
+        }
     }
 }
