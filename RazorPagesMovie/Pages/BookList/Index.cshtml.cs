fc323c4f44fd7af64844ac1b40fab933d7bca6cc
AndyJPG <jiangpeigeng@gmail.com>
2021-03-14 10:38:13 +1100

    Implemented delete functionality. Completed basic CURD application.

--- RazorPagesMovie/Pages/BookList/Index.cshtml.cs
+++ RazorPagesMovie/Pages/BookList/Index.cshtml.cs
@@ -24,5 +24,19 @@ namespace RazorPagesMovie.Pages.BookList
         {
             Books = await _db.Book.ToListAsync();
         }
+
+        public async Task<IActionResult> OnPostDelete(int id)
+        {
+            Book book = await _db.Book.FindAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            _db.Book.Remove(book);
+            await _db.SaveChangesAsync();
+
+            return RedirectToPage("Index");
+        }
     }
 }
