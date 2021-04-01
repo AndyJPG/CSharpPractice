692f8922f6852a2945c17feff416afe0602aa420
AndyJPG <jiangpeigeng@gmail.com>
2021-04-01 23:44:24 +1100

    Passed data to view.

--- OneProject.WebSite/Pages/Portfolio.cshtml.cs
+++ OneProject.WebSite/Pages/Portfolio.cshtml.cs
@@ -4,13 +4,24 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using OneProject.WebSite.Models;
+using OneProject.WebSite.Services;
 
 namespace OneProject.WebSite.Pages
 {
     public class PortfolioModel : PageModel
     {
+        public PortfolioModel(JsonFilePortfolioService portfolioService)
+        {
+            PortfolioService = portfolioService;
+        }
+
+        public JsonFilePortfolioService PortfolioService { get; set; }
+        public List<Portfolio> Portfolios { get; set; }
+
         public void OnGet()
         {
+            Portfolios = PortfolioService.GetPortfolios();
         }
     }
 }
